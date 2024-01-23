b8c1a8cb40679cc883b295f697ac20f20d189177
danialross <92746365+danialross@users.noreply.github.com>
2024-01-22 23:33:14 -0500

    installed react router
    added routes for pages and navlinks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import Navbar from "./components/Navbar";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return <Navbar />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Navbar></Navbar>}></Route>
+        <Route path="/reserve" element={<Navbar></Navbar>}></Route>
+        <Route path="/return" element={<Navbar></Navbar>}></Route>
+        <Route path="/bookings" element={<Navbar></Navbar>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import logoIcon from "../assets/logo.jpg";
+import { NavLink } from "react-router-dom";
 
 function Navbar({ children }) {
   return (
     <>
       <nav className="bg-white border-gray-200 dark:bg-green-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="https://flowbite.com/"
+          <NavLink
+            to="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
@@ -17,7 +18,7 @@ function Navbar({ children }) {
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Rent-A-Ride
             </span>
-          </a>
+          </NavLink>
           <button
             data-collapse-toggle="navbar-default"
             type="button"
@@ -45,29 +46,28 @@ function Navbar({ children }) {
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-green-900 dark:border-gray-700">
               <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-white bg-green-700 rounded md:bg-transparent md:text-green-700 md:p-0 dark:text-white md:dark:text-green-500"
-                  aria-current="page"
+                <NavLink
+                  to="/reserve"
+                  className="block py-2 px-3 text-green-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Reserve
-                </a>
+                </NavLink>
               </li>
               <li>
-                <a
-                  href="#"
+                <NavLink
+                  to="/return"
                   className="block py-2 px-3 text-green-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Return
-                </a>
+                </NavLink>
               </li>
               <li>
-                <a
-                  href="#"
+                <NavLink
+                  to="/bookings"
                   className="block py-2 px-3 text-green-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Bookings
-                </a>
+                </NavLink>
               </li>
             </ul>
           </div>
