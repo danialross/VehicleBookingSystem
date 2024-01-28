405e6c1d6d73cfaf1a8c22481bb47934eeddb4db
danialross <92746365+danialross@users.noreply.github.com>
2024-01-27 23:54:56 -0500

    refactor
    refactor image scaling and added search bar

--- frontend/src/components/WelcomeBanner.js
+++ frontend/src/components/WelcomeBanner.js
@@ -15,14 +15,59 @@ function WelcomeBanner() {
   const randomNumber = Math.floor(Math.random() * 4);
 
   return (
-    <div className=" pt-12 flex flex-col justify-center items-center">
-      <div className="font-bebas text-white text-6xl">
-        {phrases[randomNumber]}
+    <>
+      <div className="flex pt-10 flex-col justify-center items-center">
+        <div className="font-bebas text-white text-8xl text-center px-5">
+          {phrases[randomNumber]}
+        </div>
+        <div className="w-1/4 min-w-72 py-10">
+          <form>
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                placeholder="Search Reservations"
+                required
+              />
+              <button
+                type="submit"
+                className="text-white absolute end-2.5 bottom-2.5 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div className="max-w-3xl min-w-md p-5">
+          <img src={cars[randomNumber]} alt={"Car"} />
+        </div>
       </div>
-      <div className="bg-green-600">
-        <img src={cars[randomNumber]} alt={"Car"} className="max-w-3xl" />
-      </div>
-    </div>
+    </>
   );
 }
 
