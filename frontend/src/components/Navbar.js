97a4a78c115780ea67190e6f9833fbef08a6566c
danialross <92746365+danialross@users.noreply.github.com>
2024-01-31 11:24:41 -0500

    refactor
    fixed dropdown menu for smaller screens

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,13 @@
 import logoIcon from "../assets/logo.jpg";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar({ children }) {
+  const [isDropDownActive, setIsDropDownActive] = useState(false);
+
+  const toggleDropDown = () => {
+    setIsDropDownActive(!isDropDownActive);
+  };
   return (
     <>
       <nav className="bg-white border-gray-200 dark:bg-green-900">
@@ -15,11 +21,12 @@ function Navbar({ children }) {
               className="rounded-lg overflow-hidden h-12"
               alt="Car Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white hover:text-green-500">
               Rent-A-Ride
             </span>
           </NavLink>
           <button
+            onClick={toggleDropDown}
             data-collapse-toggle="navbar-default"
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -43,7 +50,12 @@ function Navbar({ children }) {
               />
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div
+            className={`${
+              isDropDownActive ? "block" : "hidden"
+            } w-full md:block md:w-auto`}
+            id="navbar-default"
+          >
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-green-900 dark:border-gray-700">
               <li>
                 <NavLink
--- frontend/src/components/SearchAndView.js
+++ frontend/src/components/SearchAndView.js
@@ -3,21 +3,21 @@ function SearchAndView() {
     <div className="flex justify-center">
       <div className="w-2/3 px-4">
         <div className="flex justify-start py-10">
-          <form class="flex items-center bg-green-900 p-5 rounded-xl">
-            <div class="relative w-full">
+          <form className="flex items-center bg-green-900 p-5 rounded-xl">
+            <div className="relative w-full">
               <input
                 type="text"
                 id="simple-search"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
                 placeholder="Search"
               />
             </div>
             <button
               type="submit"
-              class="p-2.5 ms-2 text-sm font-medium text-white bg-green-700 rounded-lg border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              className="p-2.5 ms-2 text-sm font-medium text-white bg-green-700 rounded-lg border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
             >
               <svg
-                class="w-4 h-4"
+                className="w-4 h-4"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -25,13 +25,13 @@ function SearchAndView() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                 />
               </svg>
-              <span class="sr-only">Search</span>
+              <span className="sr-only">Search</span>
             </button>
           </form>
         </div>
