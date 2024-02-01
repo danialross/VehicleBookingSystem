886ae431f5005befcc7a9ab061e4fd7271d4abfe
danialross <92746365+danialross@users.noreply.github.com>
2024-02-01 15:44:19 -0500

    gallery
    added search bar for users to search for make or model and sections of images and labels

--- frontend/src/components/SearchAndView.js
+++ frontend/src/components/SearchAndView.js
@@ -1,16 +1,58 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function SearchAndView() {
+  const url = "http://localhost:8080/images";
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("");
+  const [inputField, setInputField] = useState("");
+  const [cars, setCars] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const changeSearch = (e) => {
-    setSearch(e.target.value);
+  useEffect(() => {
+    fetchData(true);
+  }, []);
+
+  const handleChange = (e) => {
+    setInputField(e.target.value);
   };
 
-  const fetchData = () => {
-    setQuery(search);
-    // get data from db
+  const validateSearch = () => {
+    if (inputField === search || inputField === "") {
+      return false;
+    }
+
+    setSearch(inputField);
+    return true;
+  };
+
+  //   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms)); // testing for loading bar
+
+  const fetchData = async (isInitLoad) => {
+    let criteria = "";
+
+    if (!isInitLoad) {
+      //using search bar
+      if (validateSearch() === false) {
+        return;
+      }
+      criteria = "/" + inputField.replace(" ", "/");
+    }
+
+    try {
+      setIsLoading(true);
+      //   await delay(5000); // testing for loading bar
+      const result = await axios.get(url + criteria);
+
+      const newCars = [];
+      for (let object of result.data) {
+        newCars.push(object);
+      }
+      setCars(newCars);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const handleEnterDown = (event) => {
@@ -19,19 +61,20 @@ function SearchAndView() {
       fetchData();
     }
   };
+
   return (
     <div className="flex justify-center">
-      <div className="w-2/3 px-4">
-        <div className="flex justify-start py-10">
-          <form className="flex items-center bg-green-900 p-5 rounded-xl">
-            <div className="relative w-full">
+      <div className="w-4/5 px-4 ">
+        <div className="flex py-10">
+          <form className=" flex justify-center bg-green-900 p-2 rounded-xl">
+            <div className="relative">
               <input
                 type="text"
                 id="simple-search"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
                 placeholder="Search"
-                value={search}
-                onChange={changeSearch}
+                value={inputField}
+                onChange={handleChange}
                 onKeyDown={handleEnterDown}
               />
             </div>
@@ -59,8 +102,56 @@ function SearchAndView() {
             </button>
           </form>
         </div>
-        <div className="flex items-center text-white font-bebas px-5 pb-10 text-xl">
-          {query.length !== 0 ? "Results for '" + query + "'" : null}
+        <div className="text-white font-bebas px-5 pb-10 text-xl">
+          {search.length !== 0 ? "Results for '" + search + "'" : null}
+        </div>
+        <div className="flex justify-center pb-5 flex-wrap gap-4">
+          {cars.map((car) => {
+            return (
+              <div
+                className="flex flex-col justify-end bg-green-900 p-2 rounded-2xl"
+                key={car.image}
+              >
+                {isLoading ? (
+                  <div
+                    className="flex justify-center items-center"
+                    role="status"
+                  >
+                    <svg
+                      aria-hidden="true"
+                      className="inline w-60 h-60 text-gray-200 animate-spin dark:text-gray-600 fill-green-500"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                ) : (
+                  <>
+                    <img
+                      className={
+                        "max-w-80 md:max-w-md object-cover rounded-xl "
+                      }
+                      src={car.image}
+                      alt={"Car"}
+                    />
+                    <div className="font-bebas text-white text-center pt-5 pb-3 text-3xl">
+                      {car.id.make + " " + car.id.model}
+                    </div>
+                  </>
+                )}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
