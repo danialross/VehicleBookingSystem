1eb9e7230448fa08a57858ae5d9f187885e87a1b
danialross <92746365+danialross@users.noreply.github.com>
2024-02-02 23:38:53 -0500

    banner
    component which adds image and overlaps it with text to be used as a banner

---
+++ frontend/src/components/Banner.js
@@ -0,0 +1,16 @@
+function Banner({ img, text }) {
+  return (
+    <div className="relative h-64 w-full flex justify-center items-center overflow-hidden border-t-4 border-b-4  border-white">
+      <img
+        className="absolute min-w-full min-h-full object-cover object-bottom"
+        src={img}
+        alt="background"
+      />
+      <div className="z-10 font-bebas text-3xl sm:text-5xl text-white p-15 whitespace-pre">
+        {text}
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
--- frontend/src/pages/GalleryPage.js
+++ frontend/src/pages/GalleryPage.js
@@ -1,8 +1,16 @@
+import Banner from "../components/Banner";
 import SearchAndView from "../components/SearchAndView";
+import banner from "../assets/banner1.jpg";
 
 function GalleryPage() {
   return (
     <div className="bg-green-600">
+      <Banner
+        img={banner}
+        text={
+          "From the Garage to the Open Road\nWitness Our Cars in Their Element"
+        }
+      />
       <SearchAndView />
     </div>
   );
